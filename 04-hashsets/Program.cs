dfddc389c23ce134a2c4fb761d46f4b7ab92f020
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-22 16:06:57 -0500

    Completed Instructions; still need to complete Acquiring more cars

---
+++ 04-hashsets/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hashsets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> Showroom = new HashSet<string>();
+						Showroom.Add("Safari");
+						Showroom.Add("Corolla");
+						Showroom.Add("Camry");
+						Showroom.Add("Sienna");
+						Console.WriteLine($"You have {Showroom.Count} new cars in the showroom");
+						Showroom.Add("Corolla");
+						Console.Write("These are the new cars in your showroom:");
+						foreach (string model in Showroom)
+						{
+							Console.Write($" {model}");
+						}
+						Console.WriteLine();
+
+						HashSet<string> UsedLot = new HashSet<string>();
+						UsedLot.Add("Civic");
+						UsedLot.Add("Taurus");
+						UsedLot.Add("Camry");
+						Console.WriteLine($"You have {UsedLot.Count} used cars on the lot");
+						Console.Write("These are the used cars on your lot:");
+						foreach (string car in UsedLot)
+						{
+							Console.Write($" {car}");
+						}
+						Console.WriteLine();
+
+						Showroom.UnionWith(UsedLot);
+						Console.Write("These are all the cars you have:");
+						Showroom.Remove("Safari");
+						foreach (string car in Showroom)
+						{
+							Console.Write($" {car}");
+						}
+
+        }
+    }
+}
