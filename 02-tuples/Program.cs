f0f8e5d4e305d8a48dceeb2ddf9e292cfaf1f0e2
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-22 14:10:56 -0500

    Added new project for exercise 2

---
+++ 02-tuples/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02_tuples
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<(string product, double amount, int quantity)> transactions = new List<(string, double, int)>();
+    }
+  }
+}
