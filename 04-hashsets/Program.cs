2b21134ff4d014ec1e49076c6232a861ecbcd95c
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-23 14:40:19 -0500

    Finished all hashsets exercises

--- 04-hashsets/Program.cs
+++ 04-hashsets/Program.cs
@@ -3,44 +3,79 @@ using System.Collections.Generic;
 
 namespace hashsets
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            HashSet<string> Showroom = new HashSet<string>();
-						Showroom.Add("Safari");
-						Showroom.Add("Corolla");
-						Showroom.Add("Camry");
-						Showroom.Add("Sienna");
-						Console.WriteLine($"You have {Showroom.Count} new cars in the showroom");
-						Showroom.Add("Corolla");
-						Console.Write("These are the new cars in your showroom:");
-						foreach (string model in Showroom)
-						{
-							Console.Write($" {model}");
-						}
-						Console.WriteLine();
+      HashSet<string> Showroom = new HashSet<string>();
+      Showroom.Add("Safari");
+      Showroom.Add("Corolla");
+      Showroom.Add("Camry");
+      Showroom.Add("Sienna");
+      Console.WriteLine($"You have {Showroom.Count} new cars in the showroom");
+      Showroom.Add("Corolla");
+      Console.WriteLine("These are the new cars in your showroom:");
+      foreach (string model in Showroom)
+      {
+        Console.WriteLine($" {model}");
+      }
+      Console.WriteLine();
 
-						HashSet<string> UsedLot = new HashSet<string>();
-						UsedLot.Add("Civic");
-						UsedLot.Add("Taurus");
-						UsedLot.Add("Camry");
-						Console.WriteLine($"You have {UsedLot.Count} used cars on the lot");
-						Console.Write("These are the used cars on your lot:");
-						foreach (string car in UsedLot)
-						{
-							Console.Write($" {car}");
-						}
-						Console.WriteLine();
+      HashSet<string> UsedLot = new HashSet<string>();
+      UsedLot.Add("Civic");
+      UsedLot.Add("Taurus");
+      UsedLot.Add("Camry");
+      Console.WriteLine($"You have {UsedLot.Count} used cars on the lot");
+      Console.WriteLine("These are the used cars on your lot:");
+      foreach (string car in UsedLot)
+      {
+        Console.WriteLine($" {car}");
+      }
+      Console.WriteLine();
 
-						Showroom.UnionWith(UsedLot);
-						Console.Write("These are all the cars you have:");
-						Showroom.Remove("Safari");
-						foreach (string car in Showroom)
-						{
-							Console.Write($" {car}");
-						}
+      Showroom.UnionWith(UsedLot);
+      Console.WriteLine("These are all the cars you have:");
+      Showroom.Remove("Safari");
+      foreach (string car in Showroom)
+      {
+        Console.WriteLine($" {car}");
+      }
+      Console.WriteLine();
 
-        }
+
+      HashSet<string> Junkyard = new HashSet<string>()
+      {
+        "Civic",
+        "Maxima",
+        "Taurus",
+        "Ram",
+        "Pathfinder",
+        "Aztec",
+        "Prism",
+      };
+
+      HashSet<string> clone = new HashSet<string>(Showroom);
+      clone.IntersectWith(Junkyard);
+      Console.WriteLine("These are the cars from the junkyard that you have already:");
+      foreach (string car in clone)
+      {
+        Console.WriteLine($" {car}");
+      }
+      Console.WriteLine();
+
+      Showroom.UnionWith(Junkyard);
+      Showroom.Remove("Aztec");
+      Console.WriteLine("These are all the cars you have after purchasing from the junkyard:");
+      foreach (string car in Showroom)
+      {
+        Console.WriteLine($" {car}");
+      }
+      Console.WriteLine();
+
+
+
+
+
     }
+  }
 }
