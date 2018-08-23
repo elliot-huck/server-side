94b582ed8c306639f2bf0c81e78d9b58c277e2a3
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-23 12:08:28 -0500

    Finished Partitioning

--- 07-linq/Program.cs
+++ 07-linq/Program.cs
@@ -28,7 +28,7 @@ namespace linq
       }
       Console.WriteLine();
 
-
+      // FILTERING AND RESTRICTING:
       // Which of the following numbers are multiples of 4 or 6
       List<int> numbers = new List<int>()
       {
@@ -41,6 +41,91 @@ namespace linq
         Console.WriteLine(item);
       }
       Console.WriteLine();
+
+
+      // ORDERING
+      // Order these student names alphabetically, in descending order (Z to A)
+      List<string> names = new List<string>()
+      {
+          "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+          "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+          "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+          "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+          "Francisco", "Tre"
+      };
+      List<string> descend = names.OrderByDescending(n => n).ToList();
+      Console.WriteLine("STUDENTS REVERSE ABC");
+      foreach (string item in descend)
+      {
+        Console.WriteLine(item);
+      }
+      Console.WriteLine();
+
+      // Build a collection of these numbers sorted in ascending order
+      List<int> numbers2 = new List<int>()
+      {
+          15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+      };
+      List<int> sortedNumbers2 = numbers2.OrderBy(n => n).ToList();
+      Console.WriteLine("MORE NUMBERS SORTED");
+      foreach (int item in sortedNumbers2)
+      {
+        Console.WriteLine(item);
+      }
+      Console.WriteLine();
+
+
+      // AGGREGATE
+      // Output how many numbers are in this list
+      List<int> numbers3 = new List<int>()
+      {
+          15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+      };
+      int numbers3Count = numbers3.Count();
+      Console.WriteLine($@"THERE ARE {numbers3Count} NUMBERS IN NUMBERS3
+			");
+
+      // How much money have we made?
+      List<double> purchases = new List<double>()
+      {
+          2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+      };
+      double revenue = purchases.Sum();
+      Console.WriteLine($@"WE MADE ${revenue} TODAY
+			");
+
+      // What is our most expensive product?
+      List<double> prices = new List<double>()
+      {
+          879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+      };
+      double maxPrice = prices.Max();
+      Console.WriteLine($@"OUR MOST EXPENSIVE PRODUCT COSTS ${maxPrice}
+			");
+
+      /*
+          Store each number in the following List until a perfect square
+          is detected.
+
+          Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+      */
+      List<int> wheresSquaredo = new List<int>()
+      {
+          66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+      };
+			// Math.Sqrt(n) % 1 == 0
+      List<int> beforePerfSquare = wheresSquaredo.TakeWhile(n => Math.Sqrt(n) % 1 != 0).ToList();
+      Console.WriteLine("ALL NUMBERS BEFORE A PERFECT SQUARE");
+      foreach (int item in beforePerfSquare)
+      {
+        Console.WriteLine(item);
+      }
+      Console.WriteLine();
+
+
+
+
+
 
     }
   }
