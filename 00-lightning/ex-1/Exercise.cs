476f0eea5ebe80ba23a4c38d7654f824aa2a5b5d
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-27 13:34:59 -0500

    Initial commit for first lightning exercise

---
+++ 00-lightning/ex-1/Exercise.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ex_1
+{
+    public class Exercise
+    {
+      public string Name {get; set;}
+      public string GitHubURL {get; set;}
+      public string Language {get; set;}
+    }
+}
