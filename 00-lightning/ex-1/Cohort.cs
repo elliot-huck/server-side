da5a49c346cb6614abca4c8f57ba7f3e2d5fc0de
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-27 14:11:55 -0500

    Added Cohort class

---
+++ 00-lightning/ex-1/Cohort.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace ex_1
+{
+  public class Cohort
+  {
+    public int Number { get; set; }
+    List<Student> StudentList { get; } = new List<Student>();
+    List<Instructor> InstructorList { get; } = new List<Instructor>();
+
+  }
+}
