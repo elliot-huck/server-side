791e9b7bf77b515f5290f90f283b2941d99f6c98
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-27 14:04:23 -0500

    Added instructor class for step 3

---
+++ 00-lightning/ex-1/Instructor.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace ex_1
+{
+  public class Instructor
+  {
+    private string _firstName;
+    private string _lastName;
+    public string FullName { get => $"{_firstName} {_lastName}"; }
+
+    public Instructor(string first, string last)
+    {
+      _firstName = first;
+      _lastName = last;
+    }
+
+
+  }
+}
