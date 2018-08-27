c760ee2dd72ec7558cf3026801c6d1f61f69b4c1
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-27 11:58:05 -0500

    Finished nickelback exercise

--- 09-nickelback/Program.cs
+++ 09-nickelback/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace _09_nickelback
 {
@@ -17,17 +18,33 @@ namespace _09_nickelback
       ("Money", "Pink Floyd"),
     };
 
-		foreach (var song in allSongs)
-		{
-				if (song.band == "Nickelback")
-				{
-						Console.Write("No ");
-				}
-		}
+      List<(string name, string band)> goodSongs = new List<(string, string)>();
+
+      foreach (var song in allSongs)
+      {
+        if (song.band != "Nickelback")
+        {
+          goodSongs.Add(song);
+        }
+      }
+
+      Console.WriteLine();
+      Console.WriteLine("Good Songs:");
+      foreach (var goodSong in goodSongs)
+      {
+        Console.WriteLine($"'{goodSong.name}' by {goodSong.band}");
+      }
 
 
+      List<(string name, string band)> badSongs = allSongs.Except(goodSongs).ToList();
 
-      List<(string, string)> goodSongs = new List<(string, string)>() { };
+      Console.WriteLine();
+      Console.WriteLine("Nickelback Songs:");
+      foreach (var badSong in badSongs)
+      {
+        Console.WriteLine($"'{badSong.name}' by {badSong.band}");
+      }
+      Console.WriteLine();
 
 
     }
