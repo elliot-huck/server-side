6216ee52143d83f6979ae0fc2d2fdefc9941870c
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-27 11:29:58 -0500

    Initialized allSongs hashset

---
+++ 09-nickelback/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09_nickelback
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      HashSet<(string name, string band)> allSongs = new HashSet<(string name, string band)>(){
+      ("Rockstar", "Nickelback"),
+      ("Photograph", "Nickelback"),
+      ("Stairway to Heaven", "Led Zeppelin"),
+      ("The Ocean", "Led Zeppelin"),
+      ("Free Will", "Rush"),
+      ("Spirit of the Radio", "Rush"),
+      ("Money", "Pink Floyd"),
+    };
+
+		foreach (var song in allSongs)
+		{
+				if (song.band == "Nickelback")
+				{
+						Console.Write("No ");
+				}
+		}
+
+
+
+      List<(string, string)> goodSongs = new List<(string, string)>() { };
+
+
+    }
+  }
+}
