bf73ca17d9ce5c9d9bfc531f0dd7527dc2c0ebe6
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-27 14:56:46 -0500

    Added loop to writeline exercises from a student's list

--- 00-lightning/ex-1/Program.cs
+++ 00-lightning/ex-1/Program.cs
@@ -2,40 +2,48 @@
 
 namespace ex_1
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Cohort c26 = new Cohort();
-            c26.Number = 26;
+      Cohort c26 = new Cohort();
+      c26.Number = 26;
 
-            Instructor steve = new Instructor("Steve", "Brownlee");
-            Instructor jisie = new Instructor("Jisie", "David");
-            Instructor emily = new Instructor("Emily", "Lemmon");
-            Instructor jordan = new Instructor("Jordan", "Castelloe");
-            c26.InstructorList.Add(steve);
-            c26.InstructorList.Add(jisie);
-            c26.InstructorList.Add(emily);
-            c26.InstructorList.Add(jordan);
+      Instructor steve = new Instructor("Steve", "Brownlee");
+      Instructor jisie = new Instructor("Jisie", "David");
+      Instructor emily = new Instructor("Emily", "Lemmon");
+      Instructor jordan = new Instructor("Jordan", "Castelloe");
+      c26.InstructorList.Add(steve);
+      c26.InstructorList.Add(jisie);
+      c26.InstructorList.Add(emily);
+      c26.InstructorList.Add(jordan);
 
 
-            Student elliot = new Student("Elliot", "Huck");
-            Student seth = new Student("Seth", "Dana");
-            Student michael = new Student("Michael", "Robert");
-            Student leah = new Student("Leah", "Gwin");
-            Student adelaide = new Student("Adelaide", "Yoder");
-            c26.StudentList.Add(elliot);
-            c26.StudentList.Add(seth);
-            c26.StudentList.Add(michael);
-            c26.StudentList.Add(leah);
-            c26.StudentList.Add(adelaide);
+      Student elliot = new Student("Elliot", "Huck");
+      Student seth = new Student("Seth", "Dana");
+      Student michael = new Student("Michael", "Roberts");
+      Student leah = new Student("Leah", "Gwin");
+      Student adelaide = new Student("Adelaide", "Yoder");
+      c26.StudentList.Add(elliot);
+      c26.StudentList.Add(seth);
+      c26.StudentList.Add(michael);
+      c26.StudentList.Add(leah);
+      c26.StudentList.Add(adelaide);
 
-            Exercise ex1 = new Exercise("Exercise 1", "www.github.com/one", "C#");
-            Exercise ex2 = new Exercise("Exercise 2", "www.github.com/two", "C#");
-            Exercise ex3 = new Exercise("Exercise 3", "www.github.com/three", "C#");
+      Exercise ex1 = new Exercise("Exercise 1", "www.github.com/one", "C#");
+      Exercise ex2 = new Exercise("Exercise 2", "www.github.com/two", "C#");
+      Exercise ex3 = new Exercise("Exercise 3", "www.github.com/three", "C#");
 
-            steve.Assign(ex1, elliot);
+      steve.Assign(ex1, elliot);
+			jisie.Assign(ex3, elliot);
 
-        }
+      Console.WriteLine();
+      Console.WriteLine("All Elliot's Exercises:");
+      foreach (Exercise assignment in elliot.AssignedExercises)
+      {
+        Console.WriteLine(assignment.Name);
+      }
+
     }
+  }
 }
