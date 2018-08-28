f60301950bcf40fa1b9020a5cb647fbd92d6b81d
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-28 15:59:09 -0500

    Initial commit for dream team exercise

---
+++ 12-dream-team/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _12_dream_team
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
