90c699e081a1bfbf8ab9e2881056af68ab6b9f54
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-28 15:28:01 -0500

    Completed lists of vehicles and calling methods on each one

--- 11-interfaces/Program.cs
+++ 11-interfaces/Program.cs
@@ -12,31 +12,60 @@ namespace interfaces
     {
       List<IVehicle> allVehicles = new List<IVehicle>()
       {
-        new Cessna(),
-        new Helicopter(),
         new JetSki(),
         new Minivan(),
         new Motorcycle(),
         new Pickup(),
-        new Seaplane(),
         new Submarine(),
       };
 
       // Build a collection of all vehicles that fly
-
+			List<IAir> airVehicles = new List<IAir>()
+			{
+				new Cessna(),
+				new Helicopter(),
+				new Seaplane(),
+			};
       // With a single `foreach`, have each vehicle Fly()
-
+			Console.WriteLine("Flying vehicles:");
+			foreach (IAir vehicle in airVehicles)
+			{
+					vehicle.Fly();
+			}
+			Console.WriteLine();
 
 
       // Build a collection of all vehicles that operate on roads
+			List<ILand> landVehicles = new List<ILand>()
+			{
+				new Minivan(),
+				new Motorcycle(),
+				new Pickup(),
+			};
 
       // With a single `foreach`, have each road vehicle Drive()
-
+			Console.WriteLine("Road vehicles:");
+			foreach (ILand vehicle in landVehicles)
+			{
+					vehicle.Drive();
+			}
+			Console.WriteLine();
 
 
       // Build a collection of all vehicles that operate on water
+			List<IWater> waterVehicles = new List<IWater>()
+			{
+				new JetSki(),
+				new Seaplane(),
+				new Submarine(),
+			};
 
       // With a single `foreach`, have each water vehicle Drive()
+			Console.WriteLine("Marine vehicles:");
+			foreach (IWater vehicle in waterVehicles)
+			{
+					vehicle.Drive();
+			}
     }
 
   }
