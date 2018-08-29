18decb02c73695e42cb6329c6e993b4f2efbda80
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-29 09:38:03 -0500

    Added final three students and work method calls for each

--- 12-dream-team/Program.cs
+++ 12-dream-team/Program.cs
@@ -29,13 +29,61 @@ namespace dream_team
     public void Work() => Console.WriteLine($"{FirstName} immediately starts cranking out a really complex tangle of code in {Specialty}");
   }
 
-  
+  public class Brett
+  {
+    public string FirstName { get; } = "Brett";
+    public string LastName { get; } = "Shearin";
+    public string FullName { get => $"{FirstName} {LastName}"; }
+    public string Specialty { get; set; } = "Javascript";
+    public void Work() => Console.WriteLine($"{FirstName} meticulously types each individual line of {Specialty} code");
+  }
+
+  public class Will
+  {
+    public string FirstName { get; } = "Will";
+    public string LastName { get; } = "Kimball";
+    public string FullName { get => $"{FirstName} {LastName}"; }
+    public string Specialty { get; set; } = "JSON";
+    public void Work() => Console.WriteLine($"{FirstName} pastes 73,000 lines of {Specialty} code into his project because YOLO");
+  }
+
+  public class Robert
+  {
+    public string FirstName { get; } = "Robert";
+    public string LastName { get; } = "Leedy";
+    public string FullName { get => $"{FirstName} {LastName}"; }
+    public string Specialty { get; set; } = "Javascript";
+    public void Work() => Console.WriteLine($"{FirstName} quietly types his {Specialty} code as he pines away for his former seat");
+  }
+
   class Program
   {
     static void Main(string[] args)
     {
       Elliot elliot = new Elliot();
+      Seth seth = new Seth();
+      Jordan jordan = new Jordan();
+      Brett brett = new Brett();
+      Will will = new Will();
+      Robert robert = new Robert();
+
       elliot.Work();
+      Console.WriteLine();
+
+      seth.Work();
+      Console.WriteLine();
+
+      jordan.Work();
+      Console.WriteLine();
+
+      robert.Work();
+      Console.WriteLine();
+
+      brett.Work();
+      Console.WriteLine();
+
+      will.Work();
+      Console.WriteLine();
 
     }
   }
