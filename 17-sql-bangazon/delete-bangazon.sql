aa4b2c342fc0ff2771d6da606846b784121191c2
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-09-06 16:02:33 -0500

    Completed delete script to delete and drop all tables

--- 17-sql-bangazon/delete-bangazon.sql
+++ 17-sql-bangazon/delete-bangazon.sql
@@ -1,27 +1,54 @@
-DELETE FROM Computers;
-DELETE FROM CustomerOrders;
-DELETE FROM CustomerPaymentTypes;
-DELETE FROM Customers;
-DELETE FROM Departments;
-DELETE FROM EmployeeComputers;
-DELETE FROM EmployeeTrainings;
-DELETE FROM Employees;
-DELETE FROM Orders;
-DELETE FROM PaymentTypes;
-DELETE FROM ProductTypes;
-DELETE FROM Products;
-DELETE FROM Trainings;
+/*
+Departments;
+Employees;
+Trainings;
+EmployeeTrainings;
+Computers;
+EmployeeComputers;
 
-DROP TABLE IF EXISTS Computers;
-DROP TABLE IF EXISTS CustomerOrders;
-DROP TABLE IF EXISTS CustomerPaymentTypes;
-DROP TABLE IF EXISTS Customers;
-DROP TABLE IF EXISTS Departments;
+Customers;
+ProductTypes;
+Products;
+PaymentTypes
+CustomerPaymentTypes
+Orders
+CustomerOrders
+*/
+
+DELETE FROM EmployeeComputers;
+DELETE FROM Computers;
+DELETE FROM EmployeeTrainings;
+DELETE FROM Trainings;
+DELETE FROM Employees;
+DELETE FROM Departments;
+
+DELETE FROM CustomerOrders;
+DELETE FROM Orders;
+DELETE FROM CustomerPaymentTypes;
+DELETE FROM PaymentTypes;
+DELETE FROM Products;
+DELETE FROM ProductTypes;
+DELETE FROM Customers;
+
 DROP TABLE IF EXISTS EmployeeComputers;
+DROP TABLE IF EXISTS Computers;
 DROP TABLE IF EXISTS EmployeeTrainings;
-DROP TABLE IF EXISTS Employees;
-DROP TABLE IF EXISTS Orders;
-DROP TABLE IF EXISTS PaymentTypes;
-DROP TABLE IF EXISTS ProductTypes;
-DROP TABLE IF EXISTS Products;
 DROP TABLE IF EXISTS Trainings;
+DROP TABLE IF EXISTS Employees;
+DROP TABLE IF EXISTS Departments;
+
+DROP TABLE IF EXISTS CustomerOrders;
+DROP TABLE IF EXISTS Orders;
+DROP TABLE IF EXISTS CustomerPaymentTypes;
+DROP TABLE IF EXISTS PaymentTypes;
+DROP TABLE IF EXISTS Products;
+DROP TABLE IF EXISTS ProductTypes;
+DROP TABLE IF EXISTS Customers;
+
+
+
+
+
+
+
+
--- 17-sql-bangazon/seed-bangazon.sql
+++ 17-sql-bangazon/seed-bangazon.sql
@@ -1,1 +1,192 @@
+/*
+Departments;
+Employees;
+Trainings;
+EmployeeTrainings;
+Computers;
+EmployeeComputers;
+*/
+
 INSERT INTO Departments
+VALUES
+(
+	null, 
+	"Marketing", 
+	300.00
+);
+	
+INSERT INTO Departments
+VALUES
+(
+	null, 
+	"Customer Service", 
+	800.00
+);
+
+INSERT INTO Departments
+VALUES
+(
+	null, 
+	"Information Technology", 
+	1000.00
+);
+	
+INSERT INTO Departments
+VALUES
+(
+	null, 
+	"Human Resources", 
+	100.00
+);
+
+INSERT INTO Employees
+VALUES
+(
+	null,
+	"Alice",
+	"Anderson",
+	"2018-09-06 12:00:00.000",
+	15.00,
+	0,
+	0,
+	1
+);
+
+INSERT INTO Employees
+VALUES
+(
+	null,
+	"Bob",
+	"Brown",
+	"2018-09-06 12:00:00.000",
+	13.00,
+	0,
+	0,
+	2
+);
+
+INSERT INTO Employees
+VALUES
+(
+	null,
+	"Carol",
+	"Cook",
+	"2018-09-06 12:00:00.000",
+	40000,
+	1,
+	0,
+	3
+);
+
+INSERT INTO Employees
+VALUES
+(
+	null,
+	"Dave",
+	"Davidson",
+	"2018-09-06 12:00:00.000",
+	11.00,
+	0,
+	0,
+	4
+);
+
+INSERT INTO Employees
+VALUES
+(
+	null,
+	"Emily",
+	"Evans",
+	"2018-09-06 08:00:00.000",
+	50000,
+	1,
+	1,
+	1
+);
+
+INSERT INTO Employees
+VALUES
+(
+	null,
+	"Fred",
+	"Fitzgerald",
+	"2018-09-06 08:00:00.000",
+	30000,
+	1,
+	1,
+	2
+);
+
+INSERT INTO Employees
+VALUES
+(
+	null,
+	"Gina",
+	"Gregson",
+	"2018-09-06 08:00:00.000",
+	60000,
+	1,
+	1,
+	3
+);
+
+INSERT INTO Employees
+VALUES
+(
+	null,
+	"Henry",
+	"Houdini",
+	"2018-09-06 08:00:00.000",
+	70000,
+	1,
+	1,
+	4
+);
+
+
+INSERT INTO Trainings
+VALUES
+(
+	null,
+	"Basic Training",
+	"2018-09-10 08:00:00.000",
+	"2018-09-14 08:00:00.000",
+	10
+);
+
+INSERT INTO Trainings
+VALUES
+(
+	null,
+	"Intermediate Training",
+	"2018-09-17 08:00:00.000",
+	"2018-09-21 08:00:00.000",
+	10
+);
+
+INSERT INTO Trainings
+VALUES
+(
+	null,
+	"Advanced Training",
+	"2018-09-24 08:00:00.000",
+	"2018-09-28 08:00:00.000",
+	10
+);
+
+
+
+
+
+
+/*
+Customers;
+ProductTypes;
+Products;
+PaymentTypes;
+CustomerPaymentTypes;
+Orders;
+CustomerOrders;
+*/
+
+
